92d8a8481af31a954c3b0f5a998ab43e7ab40c8a
k2works <kakimomokuri@gmail.com>
2017-01-16 11:21:57 +0900

    Step2

---
+++ src/test/java/TestFrame.java
@@ -0,0 +1,12 @@
+import junit.framework.TestCase;
+
+/**
+ * Created by k2works on 2017/01/16.
+ */
+public class TestFrame extends TestCase {
+    public TestFrame(String name)
+    {
+        super(name);
+    }
+    // public void test???
+}
