983b5ba0ec14916a84b31a8e59e608caf23f8fb2
k2works <kakimomokuri@gmail.com>
2017-01-17 09:47:31 +0900

    Step7

--- src/main/java/Frame.java
+++ src/main/java/Frame.java
@@ -4,9 +4,11 @@
 public class Frame {
     public int getScore()
     {
-        return 0;
+        return itsScore;
     }
     public  void add(int pins)
     {
+        itsScore += pins;
     }
+    private int itsScore = 0;
 }
