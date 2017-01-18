37b789dcd17f6dbb38c8aa9e2a9f536edc878593
k2works <kakimomokuri@gmail.com>
2017-01-18 12:02:59 +0900

    Step14

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -9,12 +9,22 @@ public class Game {
 
     public void add(int pins)
     {
+        itsThrows[itsCurrentThrow++]=pins;
         itsScore += pins;
     }
 
     public int scoreForFrame(int frame)
     {
-        return 0;
+        int score = 0;
+        for (int ball = 0;
+             frame > 0 && (ball < itsCurrentThrow);
+             ball+=2, frame--)
+        {
+            score += itsThrows[ball] + itsThrows[ball+1];
+        }
+        return score;
     }
     private int itsScore = 0;
+    private int[] itsThrows = new int[21];
+    private int itsCurrentThrow = 0;
 }
