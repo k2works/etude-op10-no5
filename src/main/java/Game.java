6bae5eb792479c2ab56d1ba50a47b6ab1956fe60
k2works <kakimomokuri@gmail.com>
2017-01-24 09:23:54 +0900

    Step37

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -32,6 +32,7 @@ public class Game {
             firstThrow=true;
             itsCurrentFrame++;
         }
+        itsCurrentFrame = Math.min(10, itsCurrentFrame);
     }
 
     public int scoreForFrame(int theFrame)
