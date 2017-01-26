8a9fc85a8d31011075e4e0098a4088ac83576c0b
k2works <kakimomokuri@gmail.com>
2017-01-26 10:09:29 +0900

    Step53

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -14,7 +14,7 @@ public class Game {
 
     public void add(int pins)
     {
-        itsThrows[itsCurrentThrow++]=pins;
+        itsScorer.addThrow(pins);
         itsScore += pins;
         adjustCurrentFrame(pins);
     }
@@ -38,63 +38,11 @@ public class Game {
 
     public int scoreForFrame(int theFrame)
     {
-        ball = 0;
-        int score = 0;
-        for (int currentFrame = 0;
-             currentFrame < theFrame;
-             currentFrame++)
-        {
-            if (strike())
-            {
-                score += 10 + nextTwoBallsForStrike();
-                ball++;
-            }
-            else if (spare())
-            {
-                score += 10 + nextBallForSpare();
-                ball+=2;
-            }
-            else
-            {
-                score += twoBallsInFrame();
-                ball+=2;
-            }
-        }
-        return score;
+        return itsScorer.scoreForFrame(theFrame);
     }
-
-    private int nextTwoBallsForStrike() {
-        return itsThrows[ball+1] + itsThrows[ball+2];
-    }
-
-    private int nextBallForSpare() {
-        return itsThrows[ball+2];
-    }
-
-    private boolean strike() {
-        return itsThrows[ball] == 10;
-    }
-
-    private int twoBallsInFrame()
-    {
-        return itsThrows[ball] + itsThrows[ball+1];
-    }
-
-    private boolean spare() {
-        return (itsThrows[ball] + itsThrows[ball+1]) == 10;
-    }
-
-    private int nextBall() {
-        return itsThrows[ball];
-    }
-
-    private int ball;
-    private int firstThrow;
-    private int secondThrow;
 
     private int itsScore = 0;
-    private int[] itsThrows = new int[21];
-    private int itsCurrentThrow = 0;
     private int itsCurrentFrame = 1;
     private  boolean firstThrowInFrame = true;
+    private Scorer itsScorer = new Scorer();
 }
