373a508950f02f851516e6803be96a7059b88781
k2works <kakimomokuri@gmail.com>
2017-02-04 10:03:00 +0900

    Step60

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -4,84 +4,39 @@ import java.util.Map;
  * Created by k2works on 2017/01/30.
  */
 public class Game {
-    private int[] itsThrows = new int[21];
-    private int itsCurrentThrow = 0;
-    private int itsCurrentFrame = 1;
+    private int itsCurrentFrame = 0;
     private boolean firstThrowInFrame = true;
-    private int ball;
+    private Scorer itsScorer = new Scorer();
 
     public int score() {
-        return scoreForFrame(getCurrentFrame()-1);
-    }
-
-    public int getCurrentFrame() {
-        return itsCurrentFrame;
+        return scoreForFrame(itsCurrentFrame);
     }
 
     public void add(int pins) {
-        itsThrows[itsCurrentThrow++] = pins;
+        itsScorer.addThrow(pins);
         adjustCurrentFrame(pins);
     }
 
     private void adjustCurrentFrame(int pins) {
-        if (firstThrowInFrame == true) {
-            if( pins == 10 ) // ストライク
-                itsCurrentFrame++;
-            else {
-                firstThrowInFrame = false;
-            }
-        } else {
-            firstThrowInFrame = true;
-            itsCurrentFrame++;
-        }
-        itsCurrentFrame = Math.min(11, itsCurrentFrame);
+        if (lastBallInFrame(pins))
+            advanceFrame();
+        else
+            firstThrowInFrame = false;
+    }
+
+    private boolean lastBallInFrame(int pins) {
+        return strike(pins) || (!firstThrowInFrame);
+    }
+
+    private boolean strike(int pins) {
+        return firstThrowInFrame && pins == 10;
+    }
+
+    private void advanceFrame() {
+        itsCurrentFrame = Math.min(10, itsCurrentFrame + 1);
     }
 
     public int scoreForFrame(int theFrame) {
-        ball = 0;
-        int score = 0;
-        for (int currentFrame = 0;
-                currentFrame < theFrame;
-                currentFrame++)
-        {
-            if (strike()) {
-                ball++;
-                score += 10 + itsThrows[ball] + itsThrows[ball+1];
-            }
-            else {
-                score += handleSecondThrow();
-            }
-        }
-        return score;
-    }
-
-    private boolean strike() {
-        return itsThrows[ball] == 10;
-    }
-
-    private int handleSecondThrow() {
-        int score = 0;
-        // スペアの得点計算には次のフレームの第１投が必要
-        if (spare()) {
-            ball += 2;
-            score += 10 + nextBall();
-        }
-        else {
-            score += twoBallsInFrame();
-            ball += 2;
-        }
-        return score;
-    }
-
-    private boolean spare() {
-        return (itsThrows[ball] + itsThrows[ball+1]) == 10;
-    }
-
-    private int nextBall() {
-        return itsThrows[ball];
-    }
-
-    private int twoBallsInFrame() {
-        return itsThrows[ball] + itsThrows[ball+1];
+        return itsScorer.scoreForFrame(theFrame);
     }
 }
